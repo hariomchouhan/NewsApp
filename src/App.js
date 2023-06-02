889687c15ff31094230fc0e9b4ea4cc370873f47
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-06-02 13:20:30 +0530

    components structure kiya like Navbar, News and NewsItem.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Jay Shri Ram or ye meri class component hai.
+        <NavBar />
+        <News />
       </div>
     )
   }
